d02fa9ac9f207213ecbed58f22b208a81fd96d04
Kenny Nguyen <sokenns@gmail.com>
2023-10-26 19:26:40 -0700

    Create functions for buttons and calculations

--- script.js
+++ script.js
@@ -1,25 +1,81 @@
-let numOne = 0;
-let numTwo = 0;
+let numOne = "";
+let numTwo = "";
 let operator = "";
 
-const digits = document.querySelector("#digits");
+const digitsContainer = document.querySelector("#digits");
+
+const operatorsContainer = document.querySelector("#operators");
+const operatorSymbols = ["+", "-", "*", "/", "="];
+
+const resultField = document.querySelector("#result");
+const clearButton = document.querySelector("#clear");
+
+let storedCalculation = {
+  numOne: { value: "", chosen: false },
+  numTwo: { value: "", chosen: false },
+  operator: "",
+};
+
+// Create digit buttons
 for (let i = 1; i <= 9; i++) {
   let button = document.createElement("button");
   button.textContent = i;
   button.id = i;
-  digits.appendChild(button);
+  button.addEventListener("click", () => storeDigits(button.id));
+  digitsContainer.appendChild(button);
 }
 
-function operate(numOne, numTwo, operator) {
-  if (operator === "+") {
-    return add(numOne, numTwo);
-  } else if (operator === "-") {
-    return subtract(numOne, numTwo);
-  } else if (operator === "*") {
-    return multiply(numOne, numTwo);
+// Create operator buttons
+for (let i = 0; i < operatorSymbols.length; i++) {
+  let button = document.createElement("button");
+  button.textContent = operatorSymbols[i];
+  button.id = operatorSymbols[i];
+  if (operatorSymbols[i] === "=") {
+    button.addEventListener("click", operate);
   } else {
-    return divide(numOne, numTwo);
+    button.addEventListener("click", () => storeOperator(button.id));
   }
+  operatorsContainer.appendChild(button);
+}
+
+clearButton.addEventListener("click", () => clearResult());
+
+function storeDigits(num) {
+  if (!storedCalculation.numOne.chosen) {
+    storedCalculation.numOne.value += num;
+    displayResult(storedCalculation.numOne.value);
+  } else if (!storedCalculation.numTwo.chosen) {
+    storedCalculation.numTwo.value += +num;
+    displayResult(storedCalculation.numTwo.value);
+  }
+}
+
+function storeOperator(oper) {
+  if (!storedCalculation.numOne.chosen) {
+    storedCalculation.numOne.chosen = true;
+  } else if (!storedCalculation.numTwo.chosen) {
+    storedCalculation.numTwo.chosen = true;
+  }
+  storedCalculation.operator = oper;
+  console.log(storedCalculation);
+}
+
+function operate() {
+  let calculation = 0;
+  let operator = storedCalculation.operator;
+  let numOne = storedCalculation.numOne.value;
+  let numTwo = storedCalculation.numTwo.value;
+
+  if (operator === "+") {
+    calculation += add(numOne, numTwo);
+  } else if (operator === "-") {
+    calculation += subtract(numOne, numTwo);
+  } else if (operator === "*") {
+    calculation += multiply(numOne, numTwo);
+  } else {
+    calculation += divide(numOne, numTwo);
+  }
+  displayResult(calculation);
 }
 
 function add(numOne, numTwo) {
@@ -36,4 +92,17 @@ function multiply(numOne, numTwo) {
 
 function divide(numOne, numTwo) {
   return numOne / numTwo;
+}
+
+function displayResult(num) {
+  resultField.textContent = num;
+}
+
+function clearResult() {
+  storedCalculation.operator = "";
+  storedCalculation.numOne.value = "";
+  storedCalculation.numTwo.value = "";
+  storedCalculation.numOne.chosen = false;
+  storedCalculation.numTwo.chosen = false;
+  displayResult(0);
 }
