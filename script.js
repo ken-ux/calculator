666e2d7a37ccf81229d2bf75034a0075b0395711
Kenny Nguyen <sokenns@gmail.com>
2023-10-30 15:49:17 -0700

    Calculate display using operators except for =

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let storedCalculation = {
   numOne: { value: "", chosen: false },
   numTwo: { value: "" },
   operator: "",
-  display: "",
 };
 
 clearButton.addEventListener("click", () => clearResult());
@@ -18,7 +17,7 @@ for (let i = 1; i <= 9; i++) {
   let button = document.createElement("button");
   button.textContent = i;
   button.id = i;
-    button.addEventListener("click", () => storeNum(button.id));
+  button.addEventListener("click", () => storeNum(button.id));
   numsContainer.appendChild(button);
 }
 
@@ -27,35 +26,53 @@ for (let i = 0; i < operatorSymbols.length; i++) {
   let button = document.createElement("button");
   button.textContent = operatorSymbols[i];
   button.id = operatorSymbols[i];
-  //   if (operatorSymbols[i] === "=") {
-  //     button.addEventListener("click", operate);
-  //   } else {
-  //     button.addEventListener("click", () => storeOperator(button.id));
-  //   }
+  button.addEventListener("click", () => storeOperator(button.id));
   operatorsContainer.appendChild(button);
 }
 
 function storeNum(num) {
-    if (storedCalculation.numOne.chosen === false) {
-        storedCalculation.numOne.value += num;
-        resultField.textContent = storedCalculation.numOne.value;
-    } else {
-        storedCalculation.numTwo.value += num;
-        resultField.textContent = storedCalculation.numTwo.value;
-    }
+  if (storedCalculation.numOne.chosen === false) {
+    storedCalculation.numOne.value += num;
+    resultField.textContent = storedCalculation.numOne.value;
+  } else {
+    storedCalculation.numTwo.value += num;
+    resultField.textContent = storedCalculation.numTwo.value;
+  }
+}
+
+function storeOperator(operator) {
+  if (!storedCalculation.numOne.chosen) {
+    storedCalculation.numOne.chosen = true;
+    storedCalculation.operator = operator;
+  } else if (storedCalculation.numTwo.value !== "") {
+    // do calculation
+    operate(
+      storedCalculation.operator,
+      storedCalculation.numOne.value,
+      storedCalculation.numTwo.value
+    );
+    storedCalculation.operator = operator;
+  } else {
+    alert("Please choose a second number first!");
+  }
+  console.log(storedCalculation);
 }
 
 function operate(operator, numOne, numTwo) {
-  if (operator === "+") {
-    return add(numOne, numTwo);
+  // Convert strings into numbers
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
+
+  if (operator === "=") {
+    return operate(storedCalculation.operator, numOne, numTwo);
+  } else if (operator === "+") {
+    updateStoredCalculation(add(numOne, numTwo));
   } else if (operator === "-") {
-    return subtract(numOne, numTwo);
+    updateStoredCalculation(subtract(numOne, numTwo));
   } else if (operator === "*") {
-    return multiply(numOne, numTwo);
+    updateStoredCalculation(multiply(numOne, numTwo));
   } else if (operator === "/") {
-    return divide(numOne, numTwo);
-  } else {
-    console.log("Invalid operator");
+    updateStoredCalculation(divide(numOne, numTwo));
   }
 }
 
@@ -75,15 +92,18 @@ function divide(numOne, numTwo) {
   return numOne / numTwo;
 }
 
-function displayResult(num) {
+function updateStoredCalculation(num) {
+  storedCalculation.numOne.value = num;
+  storedCalculation.numOne.chosen = true;
+  storedCalculation.numTwo.value = "";
+  storedCalculation.operator = "";
   resultField.textContent = num;
 }
 
 function clearResult() {
-    storedCalculation.numOne.value = "";
-    storedCalculation.numOne.chosen = false;
-    storedCalculation.numTwo.value = "";
-    storedCalculation.operator = "";
-    storedCalculation.display = "";
-    resultField.textContent = 0;
+  storedCalculation.numOne.value = "";
+  storedCalculation.numOne.chosen = false;
+  storedCalculation.numTwo.value = "";
+  storedCalculation.operator = "";
+  resultField.textContent = 0;
 }
